d326131fe71c8f643a488d55b4ebc864af8f10ac
Emma <emmreese@udel.edu>
2024-03-07 09:12:05 -0500

    Preload Scene

--- src/scenes/preloadScene.ts
+++ src/scenes/preloadScene.ts
@@ -11,5 +11,7 @@ export default class PreloadScene extends Phaser.Scene {
 
     create() {
         this.scene.start("MainScene");
+        this.scene.start("Scene1");
+        this.scene.start("Scene2");
     }
 }
